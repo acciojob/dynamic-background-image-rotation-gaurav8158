38d984e4355298003519cd32a964310c0ffb057e
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-12-15 14:42:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 //your JS code here. If required.
-window.matchMedia("(orientation: portrait)").addEventListener("change", e => {
-    const portrait = e.matches;
-
-   if(portrait){
-	 document.body.style.backgroundImage ="url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
-
+window.addEventListener("resize",()=>{
+  if (window.innerWidth > window.innerHeight) {
+  //  document.body.style.backgroundImage ="url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
+  document.body.setAttribute("class","landscap");   
 }
-else{
-	 document.body.style.backgroundImage ="url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
-
-}  
-});
+  else{
+ //   document.body.style.backgroundImage ="url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
+document.body.setAttribute("class","portrait"); 
+}
+})

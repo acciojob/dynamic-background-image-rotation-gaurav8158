b6f7026fb0011ff67f0cb87142e3bc3f80baf85a
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-12-15 14:53:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-//your JS code here. If required.
-window.addEventListener("resize",()=>{
-  if (window.innerWidth > window.innerHeight) {
-  //  document.body.style.backgroundImage ="url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
-  document.body.setAttribute("class","landscap");   
-}
-  else{
- //   document.body.style.backgroundImage ="url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
-document.body.setAttribute("class","portrait"); 
-}
-})
+function setBackgroundImage() {
+      var backgroundImageUrl;
+
+      if (window.innerWidth > window.innerHeight) {
+        // Landscape mode
+        backgroundImageUrl = 'https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg';
+      } else {
+        // Portrait mode
+        backgroundImageUrl = 'https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg';
+      }
+
+      document.body.style.backgroundImage = 'url("' + backgroundImageUrl + '")';
+    }
+
+    // Initial call to set the background image based on the initial screen orientation
+    setBackgroundImage();
+
+    // Listen for changes in screen orientation and update the background image accordingly
+    window.addEventListener('resize', setBackgroundImage);

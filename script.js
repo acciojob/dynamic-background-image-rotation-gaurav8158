99f70bf75e10890c09341a027c187312a6fcd99c
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-12-15 14:22:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.matchMedia("(orientation: portrait)").addEventListener("change", e => {
+    const portrait = e.matches;
+
+   if(portrait){
+	 document.body.style.backgroundImage ="url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
+
+}
+else{
+	 document.body.style.backgroundImage ="url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
+
+}  
+});
